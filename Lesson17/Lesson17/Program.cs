30ac1bfd081feb69ac32397b8c43343f1e78c6ee
Sergii <sergii.sidorov@ukr.net>
2017-06-01 11:32:13 +0300

    Join in Linq using Concat

--- Lesson17/Lesson17/Program.cs
+++ Lesson17/Lesson17/Program.cs
@@ -32,6 +32,36 @@ namespace Lesson17
     }
 
 
+    public enum Occupation
+    {
+        Developer,
+        Manager,
+        QA
+    }
+
+    public class ItPerson
+    {
+        public string Occupation { get; private set; }
+        public double Salary { get; private set; }
+        public string Name { get; private set; }
+        public string FamilyName { get; private set; }
+        public Gender Gender { get; private set; }
+        public int Age { get; private set; }
+
+        public ItPerson(string name, string familyName, Gender gender, int age, string occupation, double salary)
+        {
+            this.Name = name;
+            this.FamilyName = familyName;
+            this.Gender = gender;
+            this.Age = age;
+            this.Salary = salary;
+            this.Occupation = occupation;
+
+        }
+
+    }
+
+
     class Program
     {
 
@@ -45,14 +75,27 @@ namespace Lesson17
             return query;
         }
 
-        static void vriteToConsole(IEnumerable<Person> people)
+        static void writeToConsole(IEnumerable<string> people)
         {
             foreach (var person in people)
             {
-                Console.WriteLine(person.Name);
+                Console.WriteLine(person);
             }
         }
 
+
+        static IEnumerable<string> GetItGirlsName(List<Person> people, List<ItPerson> itpeople )
+        {
+            var query = (from person in people
+                        
+                        where person.Age > 18 && person.Gender == Gender.Female
+                        select person.Name).Concat
+                        (from per in itpeople                    
+                        where  per.Gender == Gender.Female
+                        select per.Name);
+
+            return query;
+        }
         static void Main(string[] args)
         {
             int[] arr = new int[] {1, 2, 3, 4, 5};
@@ -80,7 +123,21 @@ namespace Lesson17
             personList.Add(new Person("Ganna", "Grach", Gender.Female, 10));
             personList.Add(new Person("John", "Snow", Gender.Male, 10));
 
-            vriteToConsole(GetChildren(personList));
+
+            //-------------------------------------------
+
+            List<ItPerson> itPersonList = new List<ItPerson>();
+
+            itPersonList.Add(new ItPerson("Elvis", "Prestly", Gender.Male, 33, "Manager", 3000.0));
+            itPersonList.Add(new ItPerson("Frank", "Sinatra", Gender.Male, 44, "Developer", 3000.0));
+            itPersonList.Add(new ItPerson("Vera", "Brejneva", Gender.Female, 35, "Developer", 3000.0));
+            itPersonList.Add(new ItPerson("Justin", "Beaber", Gender.Male, 44, "Developer", 3000.0));
+            itPersonList.Add(new ItPerson("Anna", "Frank", Gender.Female, 12, "QA", 3000.0));
+            itPersonList.Add(new ItPerson("Ganna", "Grach", Gender.Female, 10, "Manager", 3000.0));
+            itPersonList.Add(new ItPerson("John", "Snow", Gender.Male, 10, "QA", 3000.0));
+
+            writeToConsole(GetItGirlsName(personList, itPersonList));
+
         }
     }
 }
