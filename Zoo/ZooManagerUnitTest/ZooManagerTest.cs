3c1ce16d6fb0fbfe10fffc06bf6a820de477288a
Sergii <sergii.sidorov@ukr.net>
2017-06-01 11:59:32 +0300

    Added ZooPark and ZooManager constructors unit test

---
+++ Zoo/ZooManagerUnitTest/ZooManagerTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ZooManager;
+
+namespace ZooManagerUnitTest
+{
+    [TestClass]
+    public class ZooManagerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ZooManagerSingleton zooManager = ZooManagerSingleton.GetInstance();
+            Assert.IsNotNull(zooManager);
+        }
+    }
+}
---
+++ Zoo/ZooUnitTest/ZooParkTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zoo;
+
+namespace ZooUnitTest
+{
+    [TestClass]
+    public class ZooParkTest
+    {
+        [TestMethod]
+        public void TestZooParkConsructor()
+        {
+            ZooPark zoo = new ZooPark();
+            Assert.AreSame(zoo.zooName, "KievZoo");
+        }
+    }
+}
